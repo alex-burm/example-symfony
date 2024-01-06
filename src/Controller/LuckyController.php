aab0fee82d3e38c431b456a6508ebf4b97e473e5
Burmistrov Alexander <ab@piogroup.net>
2024-01-06 15:13:43 +0100

    init project

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LuckyController extends AbstractController
+{
+    #[Route('/lucky/number/{page}', name: 'lucky_number', defaults: ['page' => 0])]
+    public function number(int $page): Response
+    {
+        $number = 99; // rand(0, 100);
+        $list = [1, 2, 534, 4564, 'Alex', 'test', 'ABC'];
+
+        $latestPosts = [
+        ];
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+            'list' => $list,
+            'action' => $this,
+            'latestPosts' => $latestPosts,
+        ]);
+    }
+
+    public function latestPosts($a, $b, $c): Response
+    {
+        $list = [[
+                'img' => 'https://uigator.com/bootstrap5/boland-v2.1/assets/img/img-2.jpg',
+                'name' => 'Lorem ipsum dolor sit amet ipsum dolor 1',
+                'date' => '12 Apr 2000',
+            ], [
+                'img' => 'https://uigator.com/bootstrap5/boland-v2.1/assets/img/img-2.jpg',
+                'name' => 'Lorem ipsum dolor sit amet ipsum dolor 2 ',
+                'date' => '12 Apr 2001',
+            ],
+            [
+                'img' => 'https://uigator.com/bootstrap5/boland-v2.1/assets/img/img-2.jpg',
+                'name' => 'Lorem ipsum dolor sit amet ipsum dolor 3',
+                'date' => '12 Apr 2002',
+            ],
+            [
+                'img' => 'https://uigator.com/bootstrap5/boland-v2.1/assets/img/img-2.jpg',
+                'name' => 'Lorem ipsum dolor sit amet ipsum dolor 4',
+                'date' => '12 Apr 2003',
+            ],
+        ];
+
+        return $this->render('lucky/latestPosts.html.twig', [
+            'list' => $list,
+            'a' => $a,
+            'b' => $b,
+            'c' => $c,
+        ]);
+    }
+}
---
+++ src/Twig/MathExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class MathExtension extends AbstractExtension
+{
+    public function getFunctions()
+    {
+        return [
+            // new TwigFunction('round', [$this, 'myRound']),
+            new TwigFunction(
+                'round', function ($value) {
+                    return round($value);
+                }
+            ),
+        ];
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('example', [$this, 'myExample']),
+        ];
+    }
+
+    public function myExample($value)
+    {
+        return 'This is example filter ('.$value.')';
+    }
+
+    public function myRound($value)
+    {
+        return round($value);
+    }
+}
