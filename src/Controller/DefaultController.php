7391af157a15bc0a8515753ffc402067db148976
Burmistrov Alexander <ab@piogroup.net>
2024-01-14 19:05:10 +0100

    add list of posts

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +11,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class DefaultController extends AbstractController
 {
     #[Route("/", name: "homepage")]
-    public function homepage(): Response
+    public function homepage(EntityManagerInterface $em): Response
     {
-        return $this->render('default/homepage.html.twig');
+        $posts = $em->getRepository(Post::class)->findAll();
+
+        return $this->render('default/homepage.html.twig', [
+            'posts' => $posts,
+        ]);
     }
 
     #[Route("/about", name: "about")]
