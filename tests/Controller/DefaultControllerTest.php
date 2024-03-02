5872755e80f9c2b12031f0cf57978f6eb3bb924e
Burmistrov Alexander <ab@piogroup.net>
2024-03-02 18:35:57 +0100

    Basic tests

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testMainPage(): void
+    {
+        // "client" клиент это наш тестовый браузер
+        // через который мы будем делать запросы
+        $client = static::createClient();
+
+        // послать GET запрос на главную страницу
+        $crawler = $client->request('GET', '/');
+
+        // проверяем что страница отдала успешный код ответа
+        $this->assertResponseIsSuccessful();
+
+        // проверяем на текст в заголовке
+        $this->assertSelectorTextContains('h2', 'News and Stories');
+    }
+}
