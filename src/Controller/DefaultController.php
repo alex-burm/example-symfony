8e2e09321c8722934b104cb649e51aff1b23d28c
Burmistrov Alexander <ab@piogroup.net>
2024-03-11 22:52:16 +0100

    trans

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class DefaultController extends AbstractController
 {
