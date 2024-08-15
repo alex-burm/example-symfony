2f8ee628883320b47214e02eb2de3358b6a93be0
yandex <ab@piogroup.net>
2024-08-15 12:05:51 +0200

    custom fields

--- public/js/inspinia.js
+++ public/js/inspinia.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
 
     // Fast fix bor position issue with Propper.js
     // Will be fixed in Bootstrap 4.1 - https://github.com/twbs/bootstrap/pull/24092
-    Popper.Defaults.modifiers.computeStyle.gpuAcceleration = false;
 
 
     // Add body-small class if window less than 768px
@@ -21,7 +20,6 @@ $(document).ready(function () {
     }
 
     // MetisMenu
-    var sideMenu = $('#side-menu').metisMenu();
 
     // Collapse ibox function
     $('.collapse-link').on('click', function (e) {
@@ -65,23 +63,10 @@ $(document).ready(function () {
         SmoothlyMenu();
     });
 
-    // Run menu of canvas
-    $('body.canvas-menu .sidebar-collapse').slimScroll({
-        height: '100%',
-        railOpacity: 0.9
-    });
-
     // Open close right sidebar
     $('.right-sidebar-toggle').on('click', function (e) {
         e.preventDefault();
         $('#right-sidebar').toggleClass('sidebar-open');
-    });
-
-    // Initialize slimscroll for right sidebar
-    $('.sidebar-container').slimScroll({
-        height: '100%',
-        railOpacity: 0.4,
-        wheelStep: 10
     });
 
     // Open close small chat
@@ -91,12 +76,6 @@ $(document).ready(function () {
         $('.small-chat-box').toggleClass('active');
     });
 
-    // Initialize slimscroll for small chat
-    $('.small-chat-box .content').slimScroll({
-        height: '234px',
-        railOpacity: 0.4
-    });
-
     // Small todo handler
     $('.check-link').on('click', function () {
         var button = $(this).find('i');
@@ -104,13 +83,6 @@ $(document).ready(function () {
         button.toggleClass('fa-check-square').toggleClass('fa-square-o');
         label.toggleClass('todo-completed');
         return false;
-    });
-
-    // Append config box / Only for demo purpose
-    // Uncomment on server mode to enable XHR calls
-    $.get("skin-config2.html", function (data) {
-       if (!$('body').hasClass('no-skin-config'))
-           $('body').append(data);
     });
 
     // Minimalize menu
@@ -140,10 +112,6 @@ $(document).ready(function () {
     $("[data-toggle=popover]")
         .popover();
 
-    // Add slimscroll to element
-    $('.full-height-scroll').slimscroll({
-        height: '100%'
-    })
 });
 
 // Minimalize menu when screen is less than 768px
@@ -155,70 +123,13 @@ $(window).bind("resize", function () {
     }
 });
 
-// Fixed Sidebar
-$(window).bind("load", function () {
-    if ($("body").hasClass('fixed-sidebar')) {
-        $('.sidebar-collapse').slimScroll({
-            height: '100%',
-            railOpacity: 0.9
-        });
-    }
-});
-
 
 // check if browser support HTML5 local storage
 function localStorageSupport() {
     return (('localStorage' in window) && window['localStorage'] !== null)
 }
 
-// Local Storage functions
-// Set proper body class and plugins based on user configuration
-$(document).ready(function () {
-    if (localStorageSupport()) {
 
-        var collapse = localStorage.getItem("collapse_menu");
-        var fixedsidebar = localStorage.getItem("fixedsidebar");
-        var fixednavbar = localStorage.getItem("fixednavbar");
-        var boxedlayout = localStorage.getItem("boxedlayout");
-        var fixedfooter = localStorage.getItem("fixedfooter");
-
-        var body = $('body');
-
-        if (fixedsidebar == 'on') {
-            body.addClass('fixed-sidebar');
-            $('.sidebar-collapse').slimScroll({
-                height: '100%',
-                railOpacity: 0.9
-            });
-        }
-
-        if (collapse == 'on') {
-            if (body.hasClass('fixed-sidebar')) {
-                if (!body.hasClass('body-small')) {
-                    body.addClass('mini-navbar');
-                }
-            } else {
-                if (!body.hasClass('body-small')) {
-                    body.addClass('mini-navbar');
-                }
-
-            }
-        }
-
-        if (fixednavbar == 'on') {
-            $(".navbar-static-top").removeClass('navbar-static-top').addClass('navbar-fixed-top');
-            body.addClass('fixed-nav');
-        }
-
-        if (boxedlayout == 'on') {
-            body.addClass('boxed-layout');
-        }
-
-        if (fixedfooter == 'on') {
-            $(".footer").addClass('fixed');
-        }
-    }
-});
 
 // For demo purpose - animation css script
 function animationHover(element, animation) {
