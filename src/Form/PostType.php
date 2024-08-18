fe270da5b51618f84b7efbfd64b5e964fb9d20cf
yandex <ab@piogroup.net>
2024-08-18 15:27:27 +0200

    add file preview on edit page

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -11,6 +11,8 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
 
@@ -60,6 +62,35 @@ class PostType extends AbstractType
                 }
             )
         );
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $post = $event->getData();
+
+            if ($post instanceof Post && \strlen($post->getImage()) > 0) {
+                $builder = $event->getForm();
+                $file = new \Symfony\Component\HttpFoundation\File\File($post->getImage(), false);
+                $builder->add('image', FileType::class, [
+                    'mapped' => false,
+                    'required' => false,
+                    'data' => $file,
+                    'attr' => [
+                        'accept' => 'image/png, image/jpeg, image/jpg, image/webp',
+                    ],
+                    'constraints' => [
+                        new File([
+                            'maxSize' => '2000k',
+                            'mimeTypes' => [
+                                'image/png',
+                                'image/jpeg',
+                                'image/jpg',
+                                'image/bpm',
+                                'image/webp',
+                            ],
+                        ]),
+                    ],
+                ]);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
